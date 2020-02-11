050cda802eb239ca929a821b61f1218dcb49ea3b
alancipher <alansame86@gmail.com>
2020-02-11 10:50:43 -0500

    correction

---
+++ tableau.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+struct une_s {
+
+	unsigned long a;
+	unsigned long b;
+};
+
+int main (void)
+
+{
+	int tab[5];
+
+	tab[0]= 14; // si lindice est eagale a 0 cest le 0+1 ieme element
+	tab[1]= 6;
+
+	int tab2 [5] = {16,4,-5, 22, 188};
+
+    int tab3 [5]= {0}; // on met des zero dans le tableau
+    int tab4[5]= {4}; // nous mettra 4 a indice zero et les autres cases auront 0
+   
+
+   // affichage de tableau
+    int i 
+    for (i =0; i<5; i++)
+    	printf("[%d]"\n", tab4[i]);
+    		
+
+  
+}
+
+
+
+
+
+
+ 
